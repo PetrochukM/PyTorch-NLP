3df97afae065cdb7ad67f1d0750d249d09c0517a
deepblue129 <petrochukm@gmail.com>
2018-02-28 17:16:07 -0800

    Remove _static and _templates

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = []
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -112,7 +112,7 @@ html_theme_options = {'collapse_navigation': False, 'display_version': True}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
