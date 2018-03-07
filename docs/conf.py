212d1919f047bf0b241e88c4fa3aea06bac61682
deepblue129 <petrochukm@gmail.com>
2018-03-06 18:33:06 -0800

    Update conf

--- docs/conf.py
+++ docs/conf.py
@@ -25,26 +25,10 @@ project = 'PyTorch-NLP'
 copyright = '2018, Michael Petrochuk'
 author = 'Michael Petrochuk'
 
-
-def read(*names, **kwargs):
-    with io.open(
-            os.path.join(os.path.dirname(__file__), *names), encoding=kwargs.get(
-                "encoding", "utf8")) as fp:
-        return fp.read()
-
-
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
-
-
 # The short X.Y version
-version = find_version('..', 'torchnlp', '__init__.py')
+version = torchnlp.__version__
 # The full version, including alpha/beta/rc tags
-release = find_version('..', 'torchnlp', '__init__.py')
+release = torchnlp.__version__
 
 # -- General configuration ---------------------------------------------------
 
