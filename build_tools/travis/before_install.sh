9bb1f1fcbbacc73b42a678adf3e225f3d415f8b4
deepblue129 <petrochukm@gmail.com>
2018-04-12 07:59:39 -0700

    Update before_install.sh, docs did not work for pull requests

--- build_tools/travis/before_install.sh
+++
@@ -1,7 +0,0 @@
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  TRAVIS_COMMIT_RANGE="FETCH_HEAD...$TRAVIS_BRANCH"
-fi
-git diff --name-only $TRAVIS_COMMIT_RANGE | grep -qvE '(\.md$)|(^(docs|examples))/' || {
-  echo "Only docs were updated, stopping build process."
-  exit
-}
