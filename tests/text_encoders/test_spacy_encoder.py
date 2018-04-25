fee9caf97adb69952fb9d5999989d50a38f742f4
Florian Schäfer <florian.joh.schaefer@gmail.com>
2018-04-25 09:52:56 +0200

    Add SpacyEncoder unit test for unsupported language

--- tests/text_encoders/test_spacy_encoder.py
+++ tests/text_encoders/test_spacy_encoder.py
@@ -16,3 +16,14 @@ def test_spacy_encoder_not_installed_language():
         error_message = str(e)
 
     assert error_message.startswith("Language 'fr' not found.")
+
+
+def test_spacy_encoder_unsupported_language():
+    error_message = ''
+    try:
+        SpacyEncoder([], language='python')
+    except Exception as e:
+        error_message = str(e)
+
+    assert error_message.startswith("No tokenizer available for language " +
+                                    "'python'.")
