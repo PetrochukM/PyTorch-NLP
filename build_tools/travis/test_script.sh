04a95101e74a91148904f1ee9cf3314ea858702f
Michael Petrochuk <petrochukm@gmail.com>
2019-04-02 16:14:18 -0700

    Fix travis test script

--- build_tools/travis/test_script.sh
+++ build_tools/travis/test_script.sh
@@ -17,7 +17,7 @@ if [[ "$RUN_FLAKE8" == "true" ]]; then
 fi
 
 run_tests() {
-    TEST_CMD="python -m pytest tests/ src/ --doctest-modules --verbose --durations=20"
+    TEST_CMD="python -m pytest tests/ torchnlp/ --doctest-modules --verbose --durations=20"
     if [[ "$RUN_SLOW" == "true" ]]; then
         TEST_CMD="$TEST_CMD --runslow"
     fi
