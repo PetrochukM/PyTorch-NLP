eb79cc4d6994aa94e2a8b6b4ea8a831e62f9b4e1
Michael Petrochuk <petrochukm@gmail.com>
2019-10-20 16:26:55 -0700

    Fix travis

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -27,6 +27,7 @@ pip install -r requirements.txt --progress-bar off
 pip install spacy --progress-bar off
 pip install nltk --progress-bar off
 pip install sacremoses --progress-bar off
+pip install pandas --progress-bar off
 
 # SpaCy English web model
 python -m spacy download en
