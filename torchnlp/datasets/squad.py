5593491407d98be21bd118bfacb5c5291fc6662c
hurricanelx <940846513@qq.com>
2020-05-10 10:27:16 +0800

    Squad dataset

--- torchnlp/datasets/squad.py
+++ torchnlp/datasets/squad.py
@@ -15,17 +15,20 @@ def squad_dataset(directory='data/',
     """
 
     Load the Stanford Question Answering Dataset (SQuAD) dataset.
-    Stanford Question Answering Dataset (SQuAD) is a reading comprehension dataset, consisting of questions
-    posed by crowdworkers on a set of Wikipedia articles, where the answer to every question is a segment
-    of text, or span, from the corresponding reading passage, or the question might be unanswerable.
-    SQuAD2.0 combines the 100,000 questions in SQuAD1.1 with over 50,000 unanswerable questions written
-    adversarially by crowdworkers to look similar to answerable ones. To do well on SQuAD2.0, systems must
-    not only answer questions when possible, but also determine when no answer is supported by the paragraph
+    Stanford Question Answering Dataset (SQuAD) is a reading comprehension dataset,
+    consisting of questions posed by crowdworkers on a set of Wikipedia articles,
+    where the answer to every question is a segment of text, or span,
+    from the corresponding reading passage, or the question might be unanswerable.
+    SQuAD2.0 combines the 100,000 questions in SQuAD1.1 with over 50,000 unanswerable
+    questions written adversarially by crowdworkers to look similar to answerable ones.
+    To do well on SQuAD2.0, systems must not only answer questions when possible,
+    but also determine when no answer is supported by the paragraph
     and abstain from answering.
 
     **Reference:** https://rajpurkar.github.io/SQuAD-explorer/
     **Citation:**
-    Rajpurkar, P., Jia, R. and Liang, P., 2018. Know what you don't know: Unanswerable questions for SQuAD.
+    Rajpurkar, P., Jia, R. and Liang, P., 2018.
+    Know what you don't know: Unanswerable questions for SQuAD.
     arXiv preprint arXiv:1806.03822.
 
     Args:
