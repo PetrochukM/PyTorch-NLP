9b4ee69a2dc8437d20e472d68414330417497983
wxl1999 <937823963@qq.com>
2020-05-14 18:04:20 +0800

    fix bpe encoder test under python3.5

--- tests/encoders/text/test_bytepair_encoder.py
+++ tests/encoders/text/test_bytepair_encoder.py
@@ -29,7 +29,7 @@ class TestBPETextTokenizer(unittest.TestCase):
         self.assertEqual(expect_vocab_size, encoder.vocab_size)
 
     def test_encode(self):
-        if sys.version_info.micro > 5:
+        if sys.version_info.minor > 5:
             original = 'This is a coded sentence encoded by the SubwordTextTokenizer.'
             encoder = BPEEncoder(self.corpus, from_filenames=False)
 
@@ -42,7 +42,7 @@ class TestBPETextTokenizer(unittest.TestCase):
             self.assertListEqual(expect, encode_lst)
 
     def test_decoder(self):
-        if sys.version_info.micro > 5:
+        if sys.version_info.minor > 5:
             encoded = torch.tensor([5, 6, 6, 7, 56, 32, 43, 1, 14, 1, 34, 42, 47, 32,
                                     41, 36, 14, 17, 42, 49, 50, 51, 33, 9, 52, 53, 15,
                                     14, 53, 26, 21, 54, 44, 55, 37])
